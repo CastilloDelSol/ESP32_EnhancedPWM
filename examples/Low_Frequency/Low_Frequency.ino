f07942bbde7d2e789ae48caf7e2f6881acfe2229
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-02-06 20:38:42 +0100

    Update Low_Frequency.ino
    Corrected comment to "inverted" instead of "non-inverted"

--- examples/Low_Frequency/Low_Frequency.ino
+++ examples/Low_Frequency/Low_Frequency.ino
@@ -10,7 +10,7 @@ void setup()
   Serial.begin(115200);
 
   // Initialize PWM on builtin LED, channel 0, frequency 0.5 Hz, 8-bit resolution,
-  // non-inverted output, using the LEDC_USE_REF_TICK clock.
+  // inverted output, using the LEDC_USE_REF_TICK clock.
   if (!pwm.begin(LED_BUILTIN, 0, 0.5f, 8, true, LEDC_USE_REF_TICK))
   {
     Serial.println("PWM initialization failed!");
